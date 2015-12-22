56fe1175db66e7f65f6c11ba61c0678dc65e34e9
georman <manthos@gam.gr>
2015-12-22 15:33:19 +0200

    add custom index

--- timologio_eu/hooks.py
+++ timologio_eu/hooks.py
@@ -39,6 +39,7 @@ website_context = {
 
 # application home page (will override Website Settings)
 # home_page = "login"
+home_page = "index"
 
 # website user home page (by Role)
 # role_home_page = {
