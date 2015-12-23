86dbbe049a81c24b7dbef8f4f19749a043308cde
georman <manthos@gam.gr>
2015-12-23 10:39:13 +0200

    css changes

--- timologio_eu/hooks.py
+++ timologio_eu/hooks.py
@@ -35,19 +35,6 @@ website_context = {
 # web_include_css = "/assets/timologio_eu/css/timologio_eu.css"
 # web_include_js = "/assets/timologio_eu/js/timologio_eu.js"
 
-	web_include_css ="/assets/timologio_eu/css/style.css" 
-          web_include_css ="/assets/timologio_eu/css/bootstrap.css"
-	web_include_css ="/assets/timologio_eu/css/onepage.css"
-	web_include_css ="/assets/timologio_eu/css/dark.css"
-	web_include_css ="/assets/timologio_eu/css/font-icons.css"
-	web_include_css ="/assets/timologio_eu/css/et-line.css"
-	web_include_css ="/assets/timologio_eu/css/animate.css"
-	web_include_css ="/assets/timologio_eu/css/magnific-popup.css"
-	web_include_css ="/assets/timologio_eu/css/fonts.css"
-	web_include_css ="/assets/timologio_eu/css/responsive.css" 
-	
-	web_include_js ="/assets/timologio_eu/js/jquery.js"
-	web_include_js ="/assets/timologio_eu/js/plugins.js"
 
 
 # Home Pages
