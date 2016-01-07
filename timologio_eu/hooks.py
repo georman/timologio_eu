eeb69a4c604d591580001aa131a01cfd002ba06c
georman <manthos@gam.gr>
2016-01-07 14:19:17 +0200

    fix

--- timologio_eu/hooks.py
+++ timologio_eu/hooks.py
@@ -23,7 +23,8 @@ website_context = {
 
 web_include_css = "/assets/timologio_eu/css/style.css"
 web_include_css = "/assets/timologio_eu/css/animate.css"
-
+web_include_js = "/assets/timologio_eu/js/jquery.js"
+web_include_js = "/assets/timologio_eu/js/plugins.js"
 
 # Includes in <head>
 # ------------------
